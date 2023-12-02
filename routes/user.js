7cfa2d6d9d480e66835c95a66a60e0e13a50a7e3
Rafli <rafliyulistiawan@gmail.com>
2023-12-02 21:18:34 +0700

    add api login

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 const db = require('../config/firebase')
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const key = process.env.TOKEN_SECRET_KEY;
+
 router.get('/', async (req, res) => {
   try {
     const allData = []
-    const snapshot = await db.collection('users').get();
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.get();
 
     snapshot.forEach((hasil) => {
       allData.push(hasil.data())
@@ -22,6 +27,58 @@ router.get('/', async (req, res) => {
     })
   }
 })
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('username', '==', username).get();
+
+    if (snapshot.empty) {
+      const error = new Error("Wrong username or password");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const foundUser = []
+    snapshot.forEach((hasil) => foundUser.push(hasil.data()))
+    const {
+      name: foundName,
+      username: foundUsername,
+      password: foundPassword
+    } = foundUser[0]
+
+    const checkPassword = password === foundPassword;
+    // const checkPassword = await bcrypt.compare(password, foundUser[0].password);
+
+    // Apabila password salah
+    if (!checkPassword) {
+      const error = new Error("Wrong username or password");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const token = jwt.sign({
+      name: foundName,
+      username: foundUsername
+    }, key, {
+      algorithm: "HS256",
+      expiresIn: "10d"
+    })
+
+    res.status(200).json({
+      status: "Success",
+      message: 'Login Successful',
+      data: token
+    })
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+});
 
 // router.post('/data', (req, res) => {
 //   db.settings({
