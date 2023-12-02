645aff97dedfbe846d4903d9b142b1f6a9de34fa
Rafli <rafliyulistiawan@gmail.com>
2023-12-02 22:14:20 +0700

    register dah bisa

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const db = require('../config/firebase')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const key = process.env.TOKEN_SECRET_KEY;
+const { nanoid } = require('nanoid')
 
 router.get('/', async (req, res) => {
   try {
@@ -49,8 +50,8 @@ router.post("/login", async (req, res, next) => {
       password: foundPassword
     } = foundUser[0]
 
-    const checkPassword = password === foundPassword;
-    // const checkPassword = await bcrypt.compare(password, foundUser[0].password);
+    // const checkPassword = password === foundPassword;
+    const checkPassword = await bcrypt.compare(password, foundPassword);
 
     // Apabila password salah
     if (!checkPassword) {
@@ -70,7 +71,7 @@ router.post("/login", async (req, res, next) => {
     res.status(200).json({
       status: "Success",
       message: 'Login Successful',
-      data: token
+      token
     })
   } catch (error) {
     res.status(error.statusCode || 500).json({
@@ -80,22 +81,44 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-// router.post('/data', (req, res) => {
-//   db.settings({
-//     timestampsInSnapshots: true
-//   })
-//   db.collection('karyawan').add({
-//     nama: req.body.nama,
-//     usia: req.body.usia,
-//     kota: req.body.kota,
-//     waktu: new Date()
-//   })
-//   res.send({
-//     nama: req.body.nama,
-//     usia: req.body.usia,
-//     kota: req.body.kota,
-//     waktu: new Date()
-//   })
-// })
+// Register new User
+router.post("/register", async (req, res, next) => {
+  try {
+    const { name, username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(6);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const id = nanoid()
+
+    // SELECT * FROM users WHERE username = username
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('username', '==', username).get();
+
+    // Ngecek username udah ada atau belum
+    if (!snapshot.empty) {
+      const error = new Error(`Username ${username} already exist!`);
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // Masukkin data ke table users
+    await usersRef.doc(id).set({
+      name: name ? name : username,
+      username,
+      password: hashedPassword
+    });
+
+    // Send response
+    res.status(201).json({
+      status: "Success",
+      message: "Register Successful",
+    })
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+});
 
 module.exports = router
