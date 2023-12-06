835c948f686894329bf9a08f1526adb835f73d47
rafliyulistiawan <rafliyulistiawan@gmail>
2023-12-06 23:21:26 +0700

    config ci/cd

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:node:16-alpine
+WORKDIR /app
+ENV PORT 5000
+COPY . .
+RUN npm install
+EXPOSE 5000
+CMD [ "npm", "run", "start"]
--- config/firebase.js
+++ config/firebase.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require('../secrets/service_account.json');
+const serviceAccount = require('../firebase-credentials.json');
 
 initializeApp({
   credential: cert(serviceAccount)
