b7efb3afe8d73497f917b6eae4482b8dcda51bea
rafliyulistiawan <rafliyulistiawan@gmail>
2023-12-12 00:33:46 +0700

    restructure folder

---
+++ controller/electricity.js
@@ -0,0 +1,227 @@
+const db = require('../config/firebase')
+const { Timestamp } = require("@firebase/firestore");
+const { nanoid } = require('nanoid')
+
+const averagePowerHandler = async (req, res) => {
+  try {
+    const { alat_id } = req.params;
+    const { date } = req.query;
+    const usages = []
+    const newDate = date ? new Date(date) : new Date();
+
+    // Get start time of usage
+    const startTime = newDate;
+    // Convert the time to UTC. I know it's a bad practice :(
+    startTime.setUTCHours(startTime.getUTCHours() - 7);
+    // Get end time of usages
+    const endTime = new Date(startTime);
+    endTime.setUTCDate(startTime.getUTCDate() + 1)
+    // Convert date to YYYY-MM-DD
+    const convertedDate = newDate.getUTCFullYear() + "-" +
+      (newDate.getUTCMonth() + 1) + "-" +
+      (newDate.getUTCDate() + 1)
+
+    // Get all usages data from a sensor where date == date
+    const sensorRef = db.collection('sensors').doc(alat_id);
+    const usagesRef = sensorRef.collection('electricity_usages')
+    const usagesSnapshot = await usagesRef
+      .where('timestamp', '>=', startTime)
+      .where('timestamp', '<', endTime)
+      .orderBy('timestamp')
+      .get()
+
+    // Check if there is a usage or not in a given date
+    if (usagesSnapshot.empty) {
+      const error = new Error(`No electricity usage from ${convertedDate} yet.`);
+      error.statusCode = 404;
+      throw error;
+    }
+
+    // Insert each docs to usages object
+    usagesSnapshot.forEach(usageSnapshot => {
+      const { voltage, timestamp } = usageSnapshot.data()
+      let convertedTime = timestamp.toDate()
+      convertedTime.setUTCHours(convertedTime.getUTCHours() + 7) // Convert back the time to UTC+7
+      const hourMinute = ("0" + (convertedTime.getUTCHours())).slice(-2) + "." +
+        ("0" + convertedTime.getUTCMinutes()).slice(-2)
+      usages.push({
+        _id: usageSnapshot.id,
+        voltage,
+        timestamp: hourMinute,
+      })
+    })
+
+    const result = usages.map(async usage => {
+      let powerSum = 0
+      // Query all current usages by seconds
+      const currentUsagesSnapshot = await usagesRef.doc(usage._id).collection("current_usages").get()
+      currentUsagesSnapshot.forEach((currentUsageSnapshot) => {
+        const { current } = currentUsageSnapshot.data()
+        const power = current * usage.voltage // Calculate the power from each current
+        powerSum += power; // Accumulate all power from each current
+      })
+      return { ...usage, totalPower: powerSum }
+    })
+
+    Promise.all(result).then((usageResult => {
+      const groupedUsageById = Object.entries(
+        usageResult.reduce((acc, { timestamp, totalPower }) => {
+          // Group initialization
+          if (!acc[timestamp]) acc[timestamp] = [];
+          // Grouping
+          acc[timestamp].push(totalPower);
+          return acc;
+        }, {})
+      ).map(([timestamp, totalPower]) => {
+        const avgPower = totalPower.reduce((a, b) => a + b, 0) / totalPower.length;
+        return ({ timestamp, avgPower })
+      });
+
+      res.status(200).json({
+        status: "Success",
+        message: `Successfully get all power usages on ${convertedDate}`,
+        data: groupedUsageById
+      })
+    }))
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message,
+    })
+  }
+}
+
+const averageElectricityHandler = async (req, res) => {
+  try {
+    const { alat_id } = req.params;
+    const { date } = req.query;
+    const usages = []
+    const newDate = date ? new Date(date) : new Date();
+
+    // Get start time of usage
+    const startTime = newDate;
+    // Convert the time to UTC. I know it's a bad practice :(
+    startTime.setUTCHours(startTime.getUTCHours() - 7);
+    // Get end time of usages
+    const endTime = new Date(startTime);
+    endTime.setUTCDate(startTime.getUTCDate() + 1)
+    // Convert date to YYYY-MM-DD
+    const convertedDate = newDate.getUTCFullYear() + "-" +
+      (newDate.getUTCMonth() + 1) + "-" +
+      (newDate.getUTCDate() + 1)
+
+    // Get all usages data from a sensor where date == date
+    const sensorRef = db.collection('sensors').doc(alat_id);
+    const usagesRef = sensorRef.collection('electricity_usages')
+    const usagesSnapshot = await usagesRef
+      .where('timestamp', '>=', startTime)
+      .where('timestamp', '<', endTime)
+      .orderBy('timestamp')
+      .get()
+    // const usagesSnapshot = await usagesRef.get();
+
+    // Check if there is a usage or not in a given date
+    if (usagesSnapshot.empty) {
+      const error = new Error(`No electricity usage from ${convertedDate} yet.`);
+      error.statusCode = 404;
+      throw error;
+    }
+
+    // Insert each docs to usages object
+    usagesSnapshot.forEach(usageSnapshot => {
+      const { voltage, timestamp } = usageSnapshot.data()
+      let convertedTime = timestamp.toDate()
+      convertedTime.setUTCHours(convertedTime.getUTCHours() + 7) // Convert back the time to UTC+7
+      const hourMinute = ("0" + (convertedTime.getUTCHours())).slice(-2) + "." +
+        ("0" + convertedTime.getUTCMinutes()).slice(-2)
+      usages.push({
+        _id: usageSnapshot.id,
+        voltage,
+        timestamp: hourMinute
+      })
+    })
+
+    const result = usages.map(async usage => {
+      let powerSum = 0
+      const currents = []
+      // Query all current usages by seconds
+      const currentUsagesSnapshot = await usagesRef.doc(usage._id).collection("current_usages").get()
+      currentUsagesSnapshot.forEach((currentUsageSnapshot) => {
+        const { current } = currentUsageSnapshot.data()
+        // Calculate the power from each current
+        const power = current * usage.voltage
+        // Accumulate all power from each current
+        powerSum += power;
+        // Insert each current to currents object
+        currents.push(current)
+      })
+      // Return the object
+      return { ...usage, totalPower: powerSum, currents }
+    })
+
+    Promise.all(result).then((usageResult => {
+      res.status(200).json({
+        status: "Success",
+        message: `Successfully get all power usages on ${convertedDate}`,
+        data: usageResult
+      })
+    }))
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message,
+    })
+  }
+}
+
+const sendElectricityHandler = async (req, res, next) => {
+  try {
+    const { alat_id } = req.params
+
+    // Ngambil data voltage (double) dan arus (array)
+    const { voltage, arus } = req.body;
+
+    const usageID = nanoid()
+    const sensorRef = db.collection('sensors').doc(alat_id);
+    const usageRef = sensorRef.collection('electricity_usages').doc(usageID)
+    const currentRef = usageRef.collection('current_usages')
+
+    let currentTime = Timestamp.fromDate(new Date()).toDate();
+
+    // Masukkin data waktu sama voltage ke table electricity_usages
+    await usageRef.set({
+      voltage,
+      timestamp: currentTime
+    });
+
+    const timestampID = Date.now()
+    await currentRef.doc(`arus-1_${timestampID}`).set({
+      current: arus[0],
+      currentID: db.collection('currents').doc('arus_1')
+    })
+    await currentRef.doc(`arus-2_${timestampID}`).set({
+      current: arus[1],
+      currentID: db.collection('currents').doc('arus_2')
+    })
+    await currentRef.doc(`arus-3_${timestampID}`).set({
+      current: arus[2],
+      currentID: db.collection('currents').doc('arus_3')
+    })
+
+    res.status(200).json({
+      status: "Success",
+      message: 'Data Added',
+    })
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+}
+
+module.exports = {
+  averagePowerHandler,
+  averageElectricityHandler,
+  sendElectricityHandler
+}
---
+++ controller/user.js
@@ -0,0 +1,95 @@
+const db = require('../config/firebase')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config');
+
+const loginHandler = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('username', '==', username).get();
+
+    if (snapshot.empty) {
+      const error = new Error("Wrong username or password");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const foundUser = []
+    snapshot.forEach((hasil) => foundUser.push(hasil.data()))
+    const {
+      name: foundName,
+      username: foundUsername,
+      password: foundPassword
+    } = foundUser[0]
+
+    // const checkPassword = password === foundPassword;
+    const checkPassword = await bcrypt.compare(password, foundPassword);
+
+    // Apabila password salah
+    if (!checkPassword) {
+      const error = new Error("Wrong username or password");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const token = jwt.sign({
+      name: foundName,
+      username: foundUsername
+    }, jwtSecret, {
+      algorithm: "HS256"
+    })
+
+    res.status(200).json({
+      status: "Success",
+      message: 'Login Successful',
+      token
+    })
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+}
+
+const registerHandler = async (req, res, next) => {
+  try {
+    const { name, username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(6);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // SELECT * FROM users WHERE username = username
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('username', '==', username).get();
+
+    // Ngecek username udah ada atau belum
+    if (!snapshot.empty) {
+      const error = new Error(`Username ${username} already exist!`);
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // Masukkin data ke table users
+    await usersRef.doc().set({
+      name: name ? name : username,
+      username,
+      password: hashedPassword
+    });
+
+    // Send response
+    res.status(201).json({
+      status: "Success",
+      message: "Register Successful",
+    })
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+}
+
+module.exports = { loginHandler, registerHandler }
