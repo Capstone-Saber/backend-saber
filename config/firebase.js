5a940ceeb2575f394b5642e190ef5b1623d68c6a
rafliyulistiawan <rafliyulistiawan@gmail>
2023-12-17 21:34:28 +0700

    fix env bug

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, FieldValue } = require('firebase-admin/firestore');
-const serviceAccount = require('../firebase-credentials-2.json');
+const serviceAccount = require('../firebase-credentials.json');
 const { projectId } = require('.');
 
 initializeApp({
